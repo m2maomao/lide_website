af1d979f9c5f5cf07ae19ed26f4ebd0e9d2f5eff
m2maomao <m2maomao@163.com>
2020-04-28 21:09:09 +0800

    修复产品分页

--- src/containers/products/ProductList.js
+++ src/containers/products/ProductList.js
@@ -1,9 +1,41 @@
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Breadcrumb, CardColumns, Card } from 'react-bootstrap'
 import { SearchItem, SearchInput } from 'com'
+import _ from 'lodash'
 
 export default function ProductList({ data, search }) {
+  // 父组件传入数据
   const lists = Array.isArray(data[0]) ? data[0] : data
+  // 分组总数据
+  const listTotal = _.chunk(lists, 9)
+  // 当前页
+  const [page, setPage] = useState(0)
+  // 总页数
+  const pageTotal = listTotal.length
+  // 临时数据
+  const [listTemp, setListTemp] = useState([])
+  // 是否显示分页
+  const [loadMore, setLoadMore] = useState(false)
+
+  useEffect(() => {
+    setListTemp(listTotal[page])
+    if (pageTotal > 1) {
+      setLoadMore(true)
+    } else {
+      setLoadMore(false)
+    }
+  }, [lists])
+
+  const loadMoreData = () => {
+    setListTemp([...listTemp, ...listTotal[page + 1]])
+    if ((pageTotal !== page + 2) && pageTotal > 1) {
+      setPage(page + 1)
+    } else {
+      setLoadMore(false)
+      setPage(0)
+    }
+  }
 
   return (
     <>
@@ -20,9 +52,9 @@ export default function ProductList({ data, search }) {
           <div className="lists-1 row row-cols-1 row-cols-md-3">
 
             {
-              lists && lists.length ? lists.map((item, index) => (
-                <div className="col mb-4">
-                  <Link to={`/products/${item.id}`} key={index}>
+              listTemp && listTemp.length ? listTemp.map((item, index) => (
+                <div className="col mb-4" key={index}>
+                  <Link to={`/products/${item.id}`}>
                     <Card className="item">
                       <Card.Img variant="top" src={item.image} alt={item.title} />
                       <Card.Body>
@@ -39,6 +71,16 @@ export default function ProductList({ data, search }) {
               )) : '暂无产品'
             }
           </div>
+          {
+            loadMore
+              ? (
+                <div className="load-more" onClick={loadMoreData}>
+                  <span>加载更多</span>
+                </div>
+              )
+              : ''
+          }
+
         </div>
       </div>
     </>
