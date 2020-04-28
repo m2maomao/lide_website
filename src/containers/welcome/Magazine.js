6538193b784e45add231f22f3c072f966ff5e1e5
m2maomao <m2maomao@163.com>
2020-04-29 00:32:07 +0800

    点击加载

--- src/containers/welcome/Magazine.js
+++ src/containers/welcome/Magazine.js
@@ -2,6 +2,8 @@ import { Link, Route } from 'react-router-dom'
 import { Breadcrumb, CardColumns, Card } from 'react-bootstrap'
 import { useFetch } from '@/hooks/useFetch'
 import hot from '@/assets/images/welcome/hot.png'
+import { useState, useEffect } from 'react'
+import _ from 'lodash'
 import MagazineDetail from './MagazineDetail'
 
 // import imgAcquire from '@/assets/images/welcome/imgAcquire.png'
@@ -10,6 +12,37 @@ export default function Magazine() {
   const { content } = useFetch('/home/Enterlide/magazine', {
     content: [],
   })
+
+  // 分组总数据
+  const listTotal = _.chunk(content, 2)
+  // 当前页
+  const [page, setPage] = useState(0)
+  // 总页数
+  const pageTotal = listTotal.length
+  // 临时数据
+  const [listTemp, setListTemp] = useState([])
+  // 是否显示分页
+  const [loadMore, setLoadMore] = useState(false)
+
+  const loadMoreData = () => {
+    setListTemp([...listTemp, ...listTotal[page + 1]])
+    if ((pageTotal !== page + 2) && pageTotal > 1) {
+      setPage(page + 1)
+    } else {
+      setLoadMore(false)
+      setPage(0)
+    }
+  }
+
+  useEffect(() => {
+    setListTemp(listTotal[page])
+    if (pageTotal > 1) {
+      setLoadMore(true)
+    } else {
+      setLoadMore(false)
+    }
+  }, [content])
+
 
   return (
     <>
@@ -20,15 +53,24 @@ export default function Magazine() {
         <li className="breadcrumb-item">走进立得</li>
         <Breadcrumb.Item active>《我与立得同行》</Breadcrumb.Item>
       </Breadcrumb>
-      <div className="p-1r">
+      <div className="p-1r" id="container">
         <div className="magazine-container">
           <div className="d-flex magazine-lists">
             <CardColumns>
               {
-                content.length ? content.map((item, index) => <Item key={index} isIcon={item.is_hot} data={item} />) : ''
+                listTemp && listTemp.length ? listTemp.map((item, index) => <Item key={index} isIcon={item.is_hot} data={item} />) : ''
               }
             </CardColumns>
           </div>
+          {
+            loadMore
+              ? (
+                <div className="load-more" onClick={loadMoreData}>
+                  <span>加载更多</span>
+                </div>
+              )
+              : ''
+          }
         </div>
 
       </div>
