1954706f4964e34c52f8334acd421a2d40f11a0e
m2maomao <m2maomao@163.com>
2020-05-01 02:06:30 +0800

    修复科研开发滑动BUG

--- src/containers/welcome/Evolution.js
+++ src/containers/welcome/Evolution.js
@@ -28,16 +28,16 @@ export default function Evolution() {
       console.log('jquery~~~')
       const limarquee = new Limarquee('.img-carousel')
       limarquee.render({
-        direction: 'left',
-        loop: -1,
-        scrolldelay: 0,
-        scrollamount: 50,
-        circular: true,
-        drag: true,
-        runshort: true,
-        hoverstop: true,
-        xml: false,
-        inverthover: false,
+        direction: 'left', // 滚动方向，可选 left / right / up / down
+        loop: -1, // 循环次数，-1 为无限循环
+        scrolldelay: 0, // 每次重复之前的延迟
+        scrollamount: 100, // 滚动速度，越大越快
+        circular: true, // 无缝滚动，如果为 false， 则和 marquee 效果一样
+        drag: true, // 鼠标可拖动
+        runshort: true, // 内容不足是否滚动
+        hoverstop: true, // 鼠标悬停暂停
+        xml: false, // 加载 xml 文件
+        inverthover: false, // 反向，即默认不滚动，鼠 标悬停滚动
       })
     })
   }, [])
