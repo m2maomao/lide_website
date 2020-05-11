bf02b3451f54962945a6374e89786cb94abecd62
inred <372733800@qq.com>
2020-05-11 15:14:10 +0800

    小屏幕适配

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,5 +1,6 @@
 import logos from '@/assets/images/logo.png'
+import './logo.scss'
 
 export default function Logo() {
-  return <img src={logos} alt="logo" />
+  return <img src={logos} alt="logo" className="logo" />
 }
